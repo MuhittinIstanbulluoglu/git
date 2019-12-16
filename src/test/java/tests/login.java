6bc642b307c2187204276ca5643633d3822c5b1e
Muhittin Istanbulluoglu <mistanbul94@gmail.com>
2019-12-16 21:19:35 +0100

    2ndUpdatefromLocal

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -6,6 +6,7 @@ public class login {
 
 
         System.out.println("hellofromLocal");
+        // 2nd update from local
 
         // new input from github
         System.out.println("hellofromGithub");
